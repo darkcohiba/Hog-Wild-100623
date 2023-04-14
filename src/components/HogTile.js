63ad03f277bd4b37e1c30c0813d36d045e439174
Sam <samuel.waters@gmail.com>
2023-04-14 10:40:32 -0600

    done

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -3,31 +3,40 @@ import { useState } from 'react';
 
 export default function HogTile({ name, image, weight, specialty, greased, highestMedal }) {
     const [isClicked, setIsClicked] = useState(false);
+    const [isVisible, setIsVisible] = useState(true);
 
     const handleClick = () => {
         setIsClicked(!isClicked);
     };
-    return (
-        <div onClick={handleClick} className="pigTile">
-            <h3>{name}</h3>
-            <img width={250} src={image}></img>
-            {isClicked ? (
-                <div>
-                    <p>Weight: {weight}</p>
-                    <p>Specialties: {specialty}</p>
-                    <p>Highest medal achieved: {highestMedal}</p>
-                    <p>{greased ? "Greased" : ""}</p>
-                </div>
-            ) : null}
-            {isClicked && (
-                <div>
-                    <p>Weight: {weight}</p>
-                    <p>Specialties: {specialty}</p>
-                    <p>Highest medal achieved: {highestMedal}</p>
-                    <p>{greased ? "Greased" : ""}</p>
-                </div>
-            )}
 
+    const handleClickVisible = () => {
+        setIsVisible(!isVisible);
+    };
+    return (
+        <div>
+            {/* <button  onClick={handleClickVisible} >Click to View</button> */}
+            {isVisible &&
+            <div onClick={handleClick} className="pigTile">
+                <h3>{name}</h3>
+                <img width={250} src={image}></img>
+                {isClicked ? (
+                    <div>
+                        <p>Weight: {weight}</p>
+                        <p>Specialties: {specialty}</p>
+                        <p>Highest medal achieved: {highestMedal}</p>
+                        <p>{greased ? "Greased" : ""}</p>
+                    </div>
+                ) : null}
+                {isClicked && (
+                    <div>
+                        <p>Weight: {weight}</p>
+                        <p>Specialties: {specialty}</p>
+                        <p>Highest medal achieved: {highestMedal}</p>
+                        <p>{greased ? "Greased" : ""}</p>
+                    </div>
+                )}
+            </div>
+            }
         </div>
     );
 }
